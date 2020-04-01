c27c39affb7a8c41189295ff9b5f388001caf705
Mahfooz Ahmad <mahfooz.lakhani@gmail.com>
2020-04-01 19:38:47 +0530

    Adding first java file

---
+++ src/main/java/com/mahfooz/mavenlearning/Application.java
@@ -0,0 +1,15 @@
+package com.mahfooz.mavenlearning;
+
+public class Application
+{
+	public Application()
+	{
+		System.out.println("Inside Application");
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Starting Application");
+		Application app = new Application();
+	}
+}
