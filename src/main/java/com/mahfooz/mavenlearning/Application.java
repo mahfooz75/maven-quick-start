73ed31e6f5d73a4f10bed0bee3e4186c969685ca
Mahfooz Ahmad <mahfooz.lakhani@gmail.com>
2020-04-01 20:32:23 +0530

    Changing compiler plugin to Java source 1.8

--- src/main/java/com/mahfooz/mavenlearning/Application.java
+++ src/main/java/com/mahfooz/mavenlearning/Application.java
@@ -1,4 +1,6 @@
 package com.mahfooz.mavenlearning;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Application
 {
@@ -11,5 +13,16 @@ public class Application
 	{
 		System.out.println("Starting Application");
 		Application app = new Application();
+		app.greet();
+	}
+	
+	public void greet()
+	{
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		for(String greeting:greetings)
+		{
+			System.out.println(greeting);
+		}
 	}
 }
